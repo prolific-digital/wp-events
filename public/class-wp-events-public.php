9e7069cd60aed301825f3c5ca58b31a0cf09086e
Chris Miller <miller.t.chris@gmail.com>
2021-11-06 18:37:08 -0400

    Getting all event details and displaying them on the front page.

--- public/class-wp-events-public.php
+++ public/class-wp-events-public.php
@@ -97,27 +97,33 @@ class Wp_Events_Public {
 		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/wp-events-public.js', array('jquery'), $this->version, false);
 	}
 
-	public function display_start_date($content) {
+	public function display_event_details($content) {
 
 		// Check if we're inside the main loop in a single Post.
 		if (is_singular() && in_the_loop() && is_main_query()) {
 
-			$meta_value = get_post_meta(get_the_ID(), 'start_date', true);
-			return $content . '<p>' . $meta_value . '</p>';
+			// Setting the order to be displayed.
+			$meta_fields = array(
+				'start_date',
+				'end_date',
+				'start_time',
+				'end_time',
+				'zoom_url',
+			);
+
+			// Getting all of the post meta.
+			$post_meta = get_post_meta(get_the_ID());
+
+			// Getting our post meta in the order we want it.
+			foreach ($meta_fields as $key => $val) {
+				if ($post_meta[$val][0]) {
+					echo '<p>' . $post_meta[$val][0] . '';
+				}
+			}
+
+			return $content;
 		}
 
-		return '$content';
-	}
-
-	public function display_end_date($content) {
-
-		// Check if we're inside the main loop in a single Post.
-		if (is_singular() && in_the_loop() && is_main_query()) {
-
-			$meta_value = get_post_meta(get_the_ID(), 'end_date', true);
-			return $content . '<p>' . $meta_value . '</p>';
-		}
-
-		return '$content';
+		return $content;
 	}
 }
