71adc3c1cee03cf96a67340a71b0cecf6ee2c56a
Dalton McGee <dalton.mc@gmail.com>
2021-11-07 10:41:24 -0600

    feature-zoom-api

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8a01ce9248a0fb72e85a7602fc07bf990bd5cf1c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8a01ce9248a0fb72e85a7602fc07bf990bd5cf1c',
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v5.5.0',
+            'version' => '5.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'cf814442ce0e9eebe5317d61b63ccda4b85de67a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
