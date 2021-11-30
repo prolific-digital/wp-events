18954d2bd43fddfe76d019deefdeb4195390c3c8
Dalton McGee <dalton.mc@gmail.com>
2021-11-30 08:17:50 -0600

    remove archive

--- admin/class-wp-events-admin.php
+++ admin/class-wp-events-admin.php
@@ -128,7 +128,7 @@ class Wp_Events_Admin {
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => true,
 			'can_export'            => true,
-			'has_archive'           => true,
+			'has_archive'           => false,
 			'exclude_from_search'   => false,
 			'publicly_queryable'    => true,
 			'capability_type'       => 'page',
