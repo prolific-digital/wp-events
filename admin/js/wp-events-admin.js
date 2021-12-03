7d4bd9ffe12d44a3371eef93852408bbd2da3f45
Dalton McGee <dalton.mc@gmail.com>
2021-12-02 21:26:07 -0600

    Implement monthly by week recurring

--- admin/js/wp-events-admin.js
+++ admin/js/wp-events-admin.js
@@ -1,32 +1,22 @@
-(function( $ ) {
+(function ($) {
 	'use strict';
+	$(window).load(function () {
+		const startDate = $('#acf-field_61906f08deec7');
+		const startDateSelector = $('#acf-field_61906f08deec7').next();
+		startDateSelector.change(()=>{
+			const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+			let formatDate = startDate[0].defaultValue.split('')
+			formatDate.splice(4,0,'-')
+			formatDate.splice(7,0,'-')
+			const date = new Date(formatDate.join(""));
+			date.setUTCHours(6);
+			const dayOfWeek = weekday[date.getDay()];
+			for (let i = 1; i < 6; i++) {
+				const innerHtml = $(`#acf-field_619070e138d40-${i}`).parent().html();
+				$(`#acf-field_619070e138d40-${i}`).parent().html(innerHtml.replace(/\s\w+\sof/, ` ${dayOfWeek} of`));
+			}
+		})
+	});
 
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
 
-})( jQuery );
+})(jQuery);
